1ffba882d3fe393715635fb6e145088b946b4e58
ish-xyz <ishamaraia@gmail.com>
2024-08-31 18:26:51 +0100

    WIP

--- main.tf
+++ main.tf
@@ -14,16 +14,16 @@
 # }
 
 resource "local_file" "config_file" {
-  content  = templatefile("${path.module}/templates/ca-nodes.conf.tftpl", {
+  content  = templatefile("${path.module}/templates/certs-config/ca-nodes.conf.tftpl", {
     nodes = [for instance in local.mock_instances : {
       name = instance.tags["Name"]
     }]
   })
-  filename = "${path.module}/files/ca-nodes.conf"
+  filename = "${path.module}/files/certs-config/ca-nodes.conf"
 }
 
 data "template_file" "controllers_cloud_init_config" {
-  template = file("${path.module}/templates/cloud-init-controller.yaml.tftpl")
+  template = file("${path.module}/templates/cloud-init/controller-config.yaml.tftpl")
 
   depends_on = [null_resource.generate_certs]
   
@@ -32,10 +32,6 @@ data "template_file" "controllers_cloud_init_config" {
       {
         name    = "admin.crt"
         content = base64encode(data.local_file.admin_crt.content)
-      },
-      {
-        name    = "admin.csr"
-        content = base64encode(data.local_file.admin_csr.content)
       },
       {
         name    = "admin.key"
@@ -49,7 +45,24 @@ data "template_file" "controllers_cloud_init_config" {
         name    = "ca.key"
         content = base64encode(data.local_file.ca_key.content)
       },
+      {
+        name    = "kube-api-server.crt"
+        content = base64encode(data.local_file.kube_api_server_crt.content)
+      },
+      {
+        name    = "kube-api-server.key"
+        content = base64encode(data.local_file.kube_api_server_key.content)
+      },
+      {
+        name    = "service-accounts.crt"
+        content = base64encode(data.local_file.service_accounts_crt.content)
+      },
+      {
+        name    = "service-accounts.key"
+        content = base64encode(data.local_file.service_accounts_key.content)
+      }
     ])
+    etcd_systemd_unit = ""
   }
 }
 
