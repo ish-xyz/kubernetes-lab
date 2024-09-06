ec9416fab7b8d9edb0c7cf060bd2023518a083e4
ish-xyz <ishamaraia@gmail.com>
2024-09-06 12:14:22 +0100

    add bootstrap-manager doc

--- bootstrap-manager/main.go
+++ bootstrap-manager/main.go
@@ -2,6 +2,19 @@ package main
 
 func main() {
 	// load configuration file
-	// deploy CNI
-	//
+	// deploy kubernetes manifests
+	// deploy helm charts in order
+
+	/*
+		performControlPlaneChecks: true
+		performEtcdChecks: true
+		preSwitchDeployments:
+		- chart: cilium
+		  valuesB64: ""
+		postSwitchDeployments:
+		- chart: cert-manager
+		  valuesB64: ""
+		- chart: argocd
+		  valuesB64: ""
+	*/
 }
